36459f631e0afcab5162dd62a5e740d67cce3021
Nadim alabdou <46465503+nalabdou@users.noreply.github.com>
2021-04-26 12:29:09 +0200

    init project

---
+++ nadim/src/main/java/com/nalabdou/nadim/NadimApplication.java
@@ -0,0 +1,13 @@
+package com.nalabdou.nadim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadimApplication.class, args);
+	}
+
+}
---
+++ nadim/src/test/java/com/nalabdou/nadim/NadimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalabdou.nadim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
