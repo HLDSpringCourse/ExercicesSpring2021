4d8ec346ed85024b0cafef18bc0218c0e9655638
Matthieu GRANDIERES <matthieugrandierespro@gmail.com>
2021-04-26 11:59:12 +0200

    push numero 1

---
+++ matgrds/src/test/java/org/mg/matgrds/MatgrdsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mg.matgrds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatgrdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
