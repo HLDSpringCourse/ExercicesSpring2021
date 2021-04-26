32c7bc526ab41c984300af205e4d6751083ebbc7
elghaied <elghaied.eslam@gmail.com>
2021-04-26 12:25:21 +0200

    first commit

---
+++ hugold/hugold/src/main/java/org/hld/hugold/EslamEgApplication.java
@@ -0,0 +1,13 @@
+package org.hld.hugold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EslamEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EslamEgApplication.class, args);
+	}
+
+}
