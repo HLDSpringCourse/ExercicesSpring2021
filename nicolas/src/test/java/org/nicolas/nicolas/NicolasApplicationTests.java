0851d8de5f05b23c17da15c48127a6f9ee8b3e56
Nicolas <51398652+Shikshiik@users.noreply.github.com>
2021-04-26 11:48:17 +0200

    Add files via upload

---
+++ nicolas/src/test/java/org/nicolas/nicolas/NicolasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nicolas.nicolas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
