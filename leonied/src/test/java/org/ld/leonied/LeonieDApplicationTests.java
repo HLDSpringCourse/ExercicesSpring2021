026ff6a88d5f91bfb6f183e30cd21a30965ac8d1
Léonie Dusart <leonie.dusart@gmail.comm>
2021-04-26 11:55:14 +0200

    Init Spring project

---
+++ leonied/src/test/java/org/ld/leonied/LeonieDApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ld.leonied;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonieDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
