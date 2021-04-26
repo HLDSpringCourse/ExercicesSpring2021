984f508e1e1e81c66e3c7f0e50b344ca41dd5e28
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-04-26 11:52:30 +0200

    initial

---
+++ pylspring/src/test/java/org/pyl/pylspring/PylspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pyl.pylspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PylspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
