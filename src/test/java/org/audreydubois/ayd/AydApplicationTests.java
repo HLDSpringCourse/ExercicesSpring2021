d75fede61c9fa20d6b6be6c247b67a1c1d916ef6
Audrey <ay.dubois@gmail.com>
2021-04-26 11:51:34 +0200

    first commit

---
+++ src/test/java/org/audreydubois/ayd/AydApplicationTests.java
@@ -0,0 +1,13 @@
+package org.audreydubois.ayd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
