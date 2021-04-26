fee6ba4737eb3c61516f76d06cddab6ad531e1c0
NeoNagrarok <etienne.remi@gmail.com>
2021-04-26 16:46:26 +0200

    DELETE and PUT added.

--- src/main/java/controller/ItemController.java
+++ src/main/java/controller/ItemController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,15 @@ public class ItemController {
     @GetMapping(path = "/get/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Item> getItem(@PathVariable int id) {
 		return ResponseEntity.status(HttpStatus.OK).body(new Item("First item"));
+    }
+
+	@DeleteMapping(path = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> deleteItem(@PathVariable int id) {
+		return ResponseEntity.status(HttpStatus.OK).body("{'success':true}");
+    }
+
+	@PutMapping(path = "/put/{id}/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> putItem(@PathVariable int id, @PathVariable String name) {
+		return ResponseEntity.status(HttpStatus.OK).body("{'success':true}");
     }
 }
