50dae74541f9e0f228b9d1ef7bfe9a6af6859361
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-04-27 18:07:04 +0200

    [bonus][WIP] un peu de customization de swagger

--- src/main/java/org/pyl/pylspring/configuration/SpringFoxConfig.java
+++ src/main/java/org/pyl/pylspring/configuration/SpringFoxConfig.java
@@ -5,18 +5,44 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
+@EnableSwagger2
 public class SpringFoxConfig {
 
     @Bean
-    public Docket api() {
+    public Docket itemApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.basePackage("org.pyl.pylspring.controller"))
+                .paths(regex("/items.*"))
+                .build()
+                .apiInfo(metaData())
+                .useDefaultResponseMessages(false);
+                // .operationOrdering(Comparator.comparing(op -> op.getQueryParameters().first().getName()));
+    }
+
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                    "Spring Boot REST API CEFIM 2021",
+                    "Spring Boot REST API Exercise",
+                    "1.0",
+                    "Terms of service",
+                    new Contact("PYL", "https://legeay.info", "fake@email.com"),
+                    "Apache License Version 2.0",
+                    "https://www.apache.org/licenses/LICENSE-2.0",
+                    new ArrayList()
+                );
+        return apiInfo;
     }
 }
--- src/main/java/org/pyl/pylspring/controller/ItemController.java
+++ src/main/java/org/pyl/pylspring/controller/ItemController.java
@@ -1,5 +1,11 @@
 package org.pyl.pylspring.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.pyl.pylspring.dto.ItemDTO;
 import org.pyl.pylspring.exception.APIException;
 import org.pyl.pylspring.service.ItemService;
@@ -10,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/items")
+@Api(value="items API", produces = "", consumes="" , tags="Items", protocols="GET, POST, PUT, DELETE")
 public class ItemController {
 
     private final ItemService itemService;
@@ -18,30 +25,59 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+    @ApiOperation(value = "Voir la liste de toutes les items", response = ItemDTO[].class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Liste bien receptionnée"),
+    })
     @GetMapping
     public List<ItemDTO> getAll() {
 
         return itemService.getAll();
     }
 
+    @ApiOperation(value = "Voir une item", response = ItemDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Item bien receptionnée"),
+            @ApiResponse(responseCode = "400", description = "Requete erronée"),
+            @ApiResponse(responseCode = "404", description = "Item non trouvée"),
+    })
+    @ApiParam(name = "{id}", required = true)
     @GetMapping( "/{id}")
     public ItemDTO get(@PathVariable("id") String id) throws APIException {
 
         return itemService.get(id);
     }
 
+    @ApiOperation(value = "Creer une item", response = ItemDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Item bien créée"),
+            @ApiResponse(responseCode = "400", description = "Requete erronée"),
+            @ApiResponse(responseCode = "404", description = "Code region inexistant"),
+    })
     @PostMapping
     public ItemDTO create(@RequestBody ItemDTO itemDTO) throws APIException {
 
         return itemService.create(itemDTO);
     }
 
+    @ApiOperation(value = "Modifier une item", response = ItemDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Item bien modifiée"),
+            @ApiResponse(responseCode = "400", description = "Requete erronée"),
+            @ApiResponse(responseCode = "404", description = "Item non trouvée ou Code region inexistant"),
+    })
     @PutMapping
     public ItemDTO update(@RequestBody ItemDTO itemDTO) throws APIException {
 
         return itemService.update(itemDTO);
     }
 
+    @ApiOperation(value = "Supprimer une item", response = Integer.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Item bien supprimée"),
+            @ApiResponse(responseCode = "400", description = "Requete erronée"),
+            @ApiResponse(responseCode = "404", description = "Item non trouvée"),
+    })
     @DeleteMapping( "/{id}")
     public long delete(@PathVariable("id") String id) throws APIException {
         return itemService.delete(id);
