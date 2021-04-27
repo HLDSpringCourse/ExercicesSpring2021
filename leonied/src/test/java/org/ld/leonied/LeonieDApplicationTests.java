2b196490842fe6beb69dcd0704d97ff63939242f
Léonie Dusart <leonie.dusart@gmail.comm>
2021-04-27 11:43:29 +0200

    test

--- leonied/src/test/java/org/ld/leonied/LeonieDApplicationTests.java
+++ leonied/src/test/java/org/ld/leonied/LeonieDApplicationTests.java
@@ -1,13 +1,42 @@
 package org.ld.leonied;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.ld.leonied.entity.Order;
+import org.ld.leonied.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LeonieDApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private OrderService orderService;
+
 	@Test
-	void contextLoads() {
+	void testGetOrder()
+			throws Exception {
+
+		orderService.addOrder(new Order("plouf", "Lille"));
+
+		System.out.println(orderService.getOrders().size());
+
+		mvc.perform(get("/order/1")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.name", is("plouf")))
+				.andExpect(jsonPath("$.city", is("Lille")));
 	}
 
 }
