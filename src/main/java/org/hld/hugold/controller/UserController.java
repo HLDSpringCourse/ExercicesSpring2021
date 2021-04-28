ae27384ccc6d2a8ec33a3aa4d009b3fcdd92ea1a
ClementINTI <clement.wasselet@laposte.net>
2021-04-28 10:32:10 +0200

    Exercie 4 fonctionnel mais a améliorer

--- src/main/java/org/hld/hugold/controller/UserController.java
+++ src/main/java/org/hld/hugold/controller/UserController.java
@@ -3,6 +3,8 @@ package org.hld.hugold.controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.hld.hugold.entity.User;
+import org.hld.hugold.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,36 +15,43 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
-	@GetMapping(path = "/users")
-	public List<User> getUsers(){
-		List<User> liste = new ArrayList<User>();
-		liste.add(new User(123, "michel"));
-		liste.add(new User(234, "jacques"));
-		return liste;
-	}
 	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public List<User> getUsers(){
+		return userService.getAll();
+	}
+
 	@GetMapping(path = "/{id}")
 	public User getUser(@PathVariable("id") int id) {
-		return new User(456,"Bertrand");
+		return getUser(id);
 	}
 	@PostMapping
-	public ResponseEntity<User> addUser(@RequestBody User user){	
-		return ResponseEntity.status(HttpStatus.CREATED).body(user);
+	public User addUser(@RequestBody User user){	
+		return userService.addUser(user);
 	}
 	@PutMapping
 	public User updateUser( @RequestBody User user) {
+		return userService.updateUser(user);
 		
-		return user;
 	}
 	
 	@DeleteMapping(path= "/{id}")
-		public int deleteUser(@PathVariable("id")int id) {
-		return id;
+		public User deleteUser(@PathVariable("id")int id) {
+		return userService.deleteUser(id);
 	}
 	
+
 }
