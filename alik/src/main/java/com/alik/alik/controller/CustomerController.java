99faef799d2d7799fa0aa59fb28814c9ea534a64
ALIKHODR <alikhodr.i@hotmail.com>
2021-04-29 09:08:21 +0200

    add view template

--- alik/src/main/java/com/alik/alik/controller/CustomerController.java
+++ alik/src/main/java/com/alik/alik/controller/CustomerController.java
@@ -6,6 +6,7 @@ import com.alik.alik.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
---
+++ alik/src/main/java/com/alik/alik/controller/ViewController.java
@@ -0,0 +1,47 @@
+package com.alik.alik.controller;
+
+import com.alik.alik.entity.Customer;
+import com.alik.alik.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/view")
+public class ViewController {
+
+    @Autowired
+    private CustomerService service;
+
+    @GetMapping
+    public String index(Model model){
+        List<Customer> customers = service.getCustomers();
+        model.addAttribute("customers",customers);
+        model.addAttribute("newCustomer",new Customer());
+        return "index";
+    }
+
+    @DeleteMapping (path = "{id}")
+    public String delete(@PathVariable("id") int id){
+        service.deleteCustomer(id);
+        return "redirect:/view";
+    }
+
+    @PostMapping()
+    public String add(@ModelAttribute Customer customer) {
+        service.addCustomer(customer);
+        return "redirect:/view";
+    }
+
+    @GetMapping("{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        Customer customer = service.getCustomerById(id);
+        model.addAttribute("customer",customer);
+        return "show";
+    }
+
+}
